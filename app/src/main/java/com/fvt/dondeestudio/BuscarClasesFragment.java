f3588e8830fb040e175486de1dca4949d342c63f
Valentin Fontana <valefontana15@gmail.com>
2023-02-16 22:49:01 -0300

    Agregado comportamiento de buscador de clases

--- app/src/main/java/com/fvt/dondeestudio/BuscarClasesFragment.java
+++ app/src/main/java/com/fvt/dondeestudio/BuscarClasesFragment.java
@@ -3,12 +3,17 @@ package com.fvt.dondeestudio;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-import com.fvt.dondeestudio.helpers.NotificacionHelper;
+import com.fvt.dondeestudio.DTO.ClaseDTO;
+import com.fvt.dondeestudio.databinding.FragmentBuscarClasesBinding;
+import com.fvt.dondeestudio.helpers.UbicacionUtil;
+import com.google.android.gms.maps.model.LatLng;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -21,7 +26,7 @@ public class BuscarClasesFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
+    FragmentBuscarClasesBinding binding;
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
@@ -29,16 +34,6 @@ public class BuscarClasesFragment extends Fragment {
     public BuscarClasesFragment() {
         // Required empty public constructor
     }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment BuscarClasesFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static BuscarClasesFragment newInstance(String param1, String param2) {
         BuscarClasesFragment fragment = new BuscarClasesFragment();
         Bundle args = new Bundle();
@@ -60,11 +55,30 @@ public class BuscarClasesFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        NotificacionHelper nHelper = new NotificacionHelper();
-        nHelper.showNotification(getActivity(), "Hola ", "Tenes una notificion", null, null);
-        return inflater.inflate(R.layout.fragment_buscar_clases, container, false);
-
+       binding = FragmentBuscarClasesBinding.inflate(inflater, container, false);
+       Button boton = binding.btnSearch;
+       boton.setOnClickListener(e ->{
+           Bundle bundle = new Bundle();
+           ClaseDTO claseDTO = new ClaseDTO();
+           if(!binding.asignatura.getText().toString().equals(""))
+            claseDTO.setAsignatura(binding.asignatura.getText().toString());
+           claseDTO.setNivel(binding.nivel.getSelectedItem().toString());
+           claseDTO.setTarifaHoraMax(Double.valueOf(binding.tarifa.getProgress()));
+           claseDTO.setRadioMaxMetros(Double.valueOf(binding.distancia.getProgress()));
+           claseDTO.setValoracionProfesor(binding.valoracion.getProgress());
+           claseDTO.setTipo(binding.tipo.getSelectedItem().toString());
+           if(claseDTO.getTipo().equals("Presencial")){
+               UbicacionUtil.obtenerUbicacionActual(this.getActivity(), new UbicacionUtil.LocationResultCallback() {
+                   @Override
+                   public void onLocationResult(LatLng location) {
+                       claseDTO.setUbicacion(location);
+                   }
+               });
+           }
+           bundle.putSerializable("filtro", claseDTO);
+           Navigation.findNavController(binding.getRoot()).navigate(R.id.action_buscarClasesFragment_to_resultadosBusqueda, bundle);
+        });
+        return binding.getRoot();
 
     }
 }
