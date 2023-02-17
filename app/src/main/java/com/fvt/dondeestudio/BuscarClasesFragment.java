8e05c1fa5c2d406b341df66f2e950a326c54f00b
fedeTourn <fede450@outlook.com>
2023-02-16 19:09:29 -0500

    Agrega Recyclers, corrige navegacion y drawer

--- app/src/main/java/com/fvt/dondeestudio/BuscarClasesFragment.java
+++ app/src/main/java/com/fvt/dondeestudio/BuscarClasesFragment.java
@@ -2,57 +2,42 @@ package com.fvt.dondeestudio;
 
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link BuscarClasesFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.fvt.dondeestudio.model.Clase;
+
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+import java.util.List;
+
 public class BuscarClasesFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
     public BuscarClasesFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment BuscarClasesFragment.
-     */
+
     // TODO: Rename and change types and number of parameters
-    public static BuscarClasesFragment newInstance(String param1, String param2) {
+    /*
+    public static BuscarClasesFragment newInstance() {
         BuscarClasesFragment fragment = new BuscarClasesFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
+
         return fragment;
     }
+     */
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -60,5 +45,40 @@ public class BuscarClasesFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_buscar_clases, container, false);
+    }
+
+
+
+    public static class ClasesBuscadasRecyclerAdapter extends RecyclerView.Adapter<ClasesBuscadasRecyclerAdapter.ClasesBuscadasViewHolder> {
+        private List<Clase> mDataSet;
+
+        public static class ClasesBuscadasViewHolder extends RecyclerView.ViewHolder{
+            CardView card;
+            //Todo: agregar todos los demas
+            //materia
+            //direccion
+            //fechahora
+            //nombreprofesor
+
+            public ClasesBuscadasViewHolder(@NonNull View v){
+                super (v);
+                card = v.findViewById(R.id.cardClaseBuscada);
+            }
+        }
+        public ClasesBuscadasRecyclerAdapter(List<Clase> myDataSet){ mDataSet = myDataSet;}
+
+        public ClasesBuscadasRecyclerAdapter.ClasesBuscadasViewHolder
+        onCreateViewHolder(ViewGroup prn, int tipo){
+            View v = LayoutInflater.from(prn.getContext())
+                    .inflate(R.layout.item_clase_buscada, prn, false);
+            ClasesBuscadasViewHolder vh = new ClasesBuscadasViewHolder(v);
+            return vh;
+        }
+        public void onBindViewHolder(ClasesBuscadasViewHolder clasesBuscadasViewHolder, final int position){
+            //Todo: Obtener y setear los datos
+        }
+        public int getItemCount(){
+            return mDataSet.size();
+        }
     }
 }
--- app/src/main/java/com/fvt/dondeestudio/ClasesReservadasFragment.java
+++ app/src/main/java/com/fvt/dondeestudio/ClasesReservadasFragment.java
@@ -2,57 +2,44 @@ package com.fvt.dondeestudio;
 
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ClasesReservadasFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.fvt.dondeestudio.model.Clase;
+import com.fvt.dondeestudio.model.Profesor;
+
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+import java.util.List;
+
 public class ClasesReservadasFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     public ClasesReservadasFragment() {
-        // Required empty public constructor
     }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ClasesReservadasFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ClasesReservadasFragment newInstance(String param1, String param2) {
-        ClasesReservadasFragment fragment = new ClasesReservadasFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        //TODO: Arreglar los errores aca, capaz tiene que ver algo con la vista
+        /*
+        //setContentView(R.layout.fragment_clases_reservadas);
+        //recyclerView = (RecyclerView) findViewById(R.id.recyclerClasesReservadas);
+        recyclerView.setHasFixedSize(true);
+        //layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        //mAdapter = new ClasesReservadasRecyclerAdapter(lista de clases, this);
+        recyclerView.setAdapter(mAdapter);
+        */
     }
 
     @Override
@@ -61,4 +48,63 @@ public class ClasesReservadasFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_clases_reservadas, container, false);
     }
+
+
+
+    public static class ClasesReservadasRecyclerAdapter extends RecyclerView.Adapter<ClasesReservadasRecyclerAdapter.ClasesReservadasViewHolder> {
+    private List<Clase> mDataSet;
+
+    public static class ClasesReservadasViewHolder extends RecyclerView.ViewHolder{
+        CardView card;
+        TextView materia;
+        TextView direccion;
+        TextView estado;
+        TextView fechaHora;
+        ImageView imgProf;
+        TextView nombreProfesor;
+
+        public ClasesReservadasViewHolder(@NonNull View v){
+            super(v);
+            card = v.findViewById(R.id.cardClaseReservada);
+            materia = v.findViewById(R.id.tvMateria);
+            direccion = v.findViewById(R.id.tvDireccion);
+            estado = v.findViewById(R.id.tvEstado);
+            fechaHora = v.findViewById(R.id.tvFechaHora);
+            imgProf = v.findViewById(R.id.imgProfesor);
+            nombreProfesor = v.findViewById(R.id.tvNombreProfesor);
+        }
+
+    }
+
+    public ClasesReservadasRecyclerAdapter(List<Clase> myDataSet) { mDataSet = myDataSet; }
+
+    @Override
+    public ClasesReservadasViewHolder
+    onCreateViewHolder(ViewGroup prn, int tipo){
+        View v = LayoutInflater.from(prn.getContext())
+                .inflate(R.layout.item_clase_reservada, prn, false);
+
+        ClasesReservadasViewHolder vh= new ClasesReservadasViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(ClasesReservadasViewHolder claseHolder, final int position){
+        //Obtiene y setea los datos
+        Clase clase = mDataSet.get(position);
+        Profesor profesor = clase.getProfesor();
+        claseHolder.materia.setText(clase.getAsignatura());
+        claseHolder.fechaHora.setText(clase.getHorario().toString());
+        //TODO: nombreProfesor
+        /**claseHolder.nombreProfesor.setText(profesor.getNombre()+" "+profesor.getApellido());*/
+
+        //TODO: ubicacion, ver como hacer lo de la latitud y longitud
+        //TODO: ESTADO ES COMPLICADO PQ DICE SI EL PROFESOR ACEPTO AL ALUMNO, NO SE DONDE SE GUARDARIA
+        //TODO: imagen del profesor, buscar del profesor(si tiene)
+    }
+
+    @Override
+    public int getItemCount(){ return mDataSet.size(); }
+
+}
 }
