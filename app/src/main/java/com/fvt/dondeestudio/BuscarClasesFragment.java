2a4d0b0944677d0d67ed22afce2f5b069a847571
Valentin Fontana <valefontana15@gmail.com>
2023-03-03 15:54:34 -0300

    bug fix

--- app/src/main/java/com/fvt/dondeestudio/BuscarClasesFragment.java
+++ app/src/main/java/com/fvt/dondeestudio/BuscarClasesFragment.java
@@ -54,7 +54,7 @@ public class BuscarClasesFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setHasOptionsMenu(false);
+        setHasOptionsMenu(true);
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
